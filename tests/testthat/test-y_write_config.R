9e66edd7a73f2bb9d741418e47f57aa57e065b39
akselthomsen <thomsenaksel@gmail.com>
2026-05-11 13:55:20 +0200

    test: add write_config default path coverage

--- tests/testthat/test-y_write_config.R
+++ tests/testthat/test-y_write_config.R
@@ -10,3 +10,17 @@ test_that("write_config() writes study to directory", {
   list.files(tmpdir) |>
     expect_snapshot()
 })
+
+test_that("write_config() uses study@path when path is NULL", {
+  tmpdir <- withr::local_tempdir()
+
+  file.copy(list.files(test_path("test_study"), full.names = TRUE), tmpdir)
+
+  study <- mighty_study(path = tmpdir)
+
+  write_config(x = study) |>
+    expect_no_error()
+
+  expected <- c("_mighty.yml", "_study.yml", "adae.yml", "adsl.yml", "advs.yml")
+  expect_true(all(file.exists(file.path(tmpdir, expected))))
+})
