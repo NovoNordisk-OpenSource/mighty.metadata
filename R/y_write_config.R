19f58eca19cb3d622cd709167ae5bdfbe3266040
akselthomsen <thomsenaksel@gmail.com>
2026-05-11 14:35:29 +0200

    docs: remove orphaned @param tags from write_config docs

--- R/y_write_config.R
+++ R/y_write_config.R
@@ -4,10 +4,6 @@
 #' `_mighty.yml` and `_study.yml` when non-empty.
 #'
 #' If `path` is `NULL` (default), files are written to `x@path`.
-#'
-#' @param x A `mighty_study` object.
-#' @param path `character(1)` or `NULL`. Directory to write to.
-#'   Defaults to `x@path`.
 #' @name mighty_study
 #' @rdname mighty_study
 S7::method(write_config, mighty_study) <- function(x, path = NULL) {
