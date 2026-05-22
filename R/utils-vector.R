7b96fe57152b53fde8dd59fbdd8607caeec54aba
KatarzynaGraczyk <katarzyna.graczyk@appsilon.com>
2026-05-22 15:17:58 +0200

    added check for colum -colum dependencies

--- R/utils-vector.R
+++ R/utils-vector.R
@@ -164,3 +164,32 @@ check_unique_ids <- function(x) {
     )
   }
 }
+
+#' Check that no forbidden column-to-column dependencies exist across domains
+#'
+#' Column dependencies are allowed only on row and parameters levels.
+#' Dependencies pointing directly to another column are considered invalid
+#' and will terminate execution with a descriptive error.
+check_column_dependencies <- function(mighty_domains) {
+
+  all_column_ids <- lapply(mighty_domains, function(domain) {
+
+    flattened_columns <- unlist(domain$columns)
+    local_column_ids <- flattened_columns[grepl("id$", names(flattened_columns))]
+    paste0(domain$id, ".", local_column_ids)
+  }) |> unlist()
+
+  lapply(mighty_domains, function(domain) {
+
+    flattened_columns <- unlist(domain$columns)
+    declared_dependencies <- flattened_columns[ grepl("depends", names(flattened_columns))]
+    forbidden_dependencies <- intersect( declared_dependencies, all_column_ids)
+
+    if (length(forbidden_dependencies) > 0) {
+      cli::cli_abort(paste0("Detected not allowed column to column dependencies: {forbidden_dependencies} in {domain$id} dataset. ",
+                            "Please fix this dependency or remove it."))
+    }
+  })
+
+  invisible(TRUE)
+}
