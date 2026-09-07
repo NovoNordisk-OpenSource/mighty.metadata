fdcc656fe705f02f33ad40b415243a1af93a8d55
MEWP (Matthew Phelps) <MEWP+nngithub@novonordisk.com>
2026-09-07 13:05:49 +0200

    re-add test scenario

--- tests/testthat/test-a_mighty_config.R
+++ tests/testthat/test-a_mighty_config.R
@@ -108,6 +108,20 @@ test_that("mighty_config works with in-memory data", {
   expect_null(x@file)
 })
 
+test_that("mighty_config errors on duplicate external_data ids", {
+  expect_error(
+    mighty_config(
+      .data = list(
+        external_data = list(
+          list(id = "DM", keys = "USUBJID"),
+          list(id = "DM", keys = "STUDYID")
+        )
+      )
+    ),
+    regexp = "Duplicate"
+  )
+})
+
 test_that("mighty_config requires exactly one input source", {
   expect_error(
     mighty_config(
